b771f3d7e2cf7d4f44b0671556955163f071f8a9
Hanane OUB <hanane.oublouhou@gmail.com>
2020-12-18 02:43:07 +0100

    18/12

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from SPARQLWrapper import SPARQLWrapper, JSON, POST, DIGEST, CSV
 from flask import Flask, render_template
 import json
-
+import re 
 
 app = Flask(__name__)
 
@@ -17,15 +17,17 @@ def get_data():
         PREFIX geo: <https://schema.org/GeoCoordinates/>
         PREFIX mv: <http://schema.mobivoc.org/#>
 
-        SELECT ?latitude ?longitude ?places ?type
+        SELECT ?latitude ?longitude ?id ?parking_name ?places ?type
         WHERE {
             GRAPH <http://www.semweb.com/project/parking_station>
                 {
                     ?item geo:latitude ?latitude ;
-                        geo:longitude ?longitude;
+                        geo:longitude ?longitude ;
+                        mv:id ?id ;
+                        mv:name ?parking_name ;
                         ex:hasParking 
                         [ 
-                            mv:RealTimeCapacity ?places;
+                            mv:RealTimeCapacity ?places ;
                             mv:ParkingFacility ?type 
                         ]
                 }
@@ -42,12 +44,14 @@ def get_data():
         PREFIX geo: <https://schema.org/GeoCoordinates/>
         PREFIX mv: <http://schema.mobivoc.org/#>
 
-        SELECT ?latitude ?longitude ?plug ?bool ?status
+        SELECT ?latitude ?longitude ?id ?station_name ?plug ?bool ?status
         WHERE {
             GRAPH <http://www.semweb.com/project/electric_charger>
                 {
                     ?item geo:latitude ?latitude ;
                         geo:longitude ?longitude ;
+                        mv:id ?id ;
+                        mv:name ?station_name ;
                         ex:hasPlug
                         [ 
                             mv:PlugType ?plug ;
@@ -55,14 +59,22 @@ def get_data():
                             ex:status ?status
                         ]
                 }
-        FILTER regex(?status, "Disponible")
-        }'''
+            FILTER regex(?status, "Disponible")
+
+            }'''
                     )
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
     chargers = results['results']['bindings']
+    with open("df_bornes.json") as f:
+        jsonldChargers = json.load(f)
+        # print(jsonldChargers)
+        # jsonldChargers = json.loads(jsonldChargers)
+    with open("df_parking.json") as f:
+        jsonldParks = json.load(f)
+        # jsonldParks =json.loads(jsonldChargers)
 
-    return render_template('index.html', parks=parkings, chargers=chargers)
+    return render_template('index.html', parks=parkings, chargers=chargers, jsonldChargers=jsonldChargers, jsonldParks=jsonldParks)
 
 
 if __name__ == '__main__':
--- triplestore.py
+++ triplestore.py
@@ -4,110 +4,124 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 import json
 from rdflib import URIRef, BNode, Literal, Graph, Namespace
 from rdflib.namespace import RDF, FOAF, RDFS, XSD
+import threading
+
 
 # Namespaces
 mv = Namespace('http://schema.mobivoc.org/#')
 geo = Namespace('https://schema.org/GeoCoordinates/')
 ex = Namespace('http://example.com/#')
 
+def loadDatatoServer():
+    """ Télécharger les données à partir des différentes sources
+        Créer les triples associés --> Graphs RDF
+        Enregistrer les graphes sous format ttl et sous format JSON LD
+        Stocker les triples sur le serveur Fuseki
+        Cette fonction s'exécute toutes les deux minutes afin de mettre à jour les données """
 
-# Importation des données
-#with urllib.request.urlopen("https://www.data.gouv.fr/fr/datasets/r/556d828f-2643-4bd9-97ed-38b6f4b369e5") as url:
-with urlopen("https://www.data.gouv.fr/fr/datasets/r/556d828f-2643-4bd9-97ed-38b6f4b369e5") as url:
-    df_bornes = json.loads(url.read().decode())
-#with urllib.request.urlopen("https://www.data.gouv.fr/fr/datasets/r/0fb3fe0e-2a69-45ac-aca7-6923d18698bc") as url:
-with urlopen("https://www.data.gouv.fr/fr/datasets/r/0fb3fe0e-2a69-45ac-aca7-6923d18698bc") as url:
-    df_parking = json.loads(url.read().decode())
+    # Mettre à jour les données en faisant appel toutes les deux minutes à la fonction loadDatatoServer
+    threading.Timer(120.0, loadDatatoServer).start() 
 
-# Supprimer les entrées sans géolocalisation
-def filterData(x):
-    if "geo" in x["fields"].keys():
-        return(True)
-    return(False)
+    # Importation des données
+    #with urllib.request.urlopen("https://www.data.gouv.fr/fr/datasets/r/556d828f-2643-4bd9-97ed-38b6f4b369e5") as url:
+    with urlopen("https://www.data.gouv.fr/fr/datasets/r/556d828f-2643-4bd9-97ed-38b6f4b369e5") as url:
+        df_bornes = json.loads(url.read().decode())
+    #with urllib.request.urlopen("https://www.data.gouv.fr/fr/datasets/r/0fb3fe0e-2a69-45ac-aca7-6923d18698bc") as url:
+    with urlopen("https://www.data.gouv.fr/fr/datasets/r/0fb3fe0e-2a69-45ac-aca7-6923d18698bc") as url:
+        df_parking = json.loads(url.read().decode())
 
-df_parking = list(filter(filterData, df_parking))
+    # Supprimer les entrées sans géolocalisation
+    def filterData(x):
+        if "geo" in x["fields"].keys():
+            return(True)
+        return(False)
 
-# Création des graphes
-graphE = Graph(identifier="ElectricChargers")
-graphP = Graph(identifier="Parkings")
+    df_parking = list(filter(filterData, df_parking))
 
-# Création des namespaces dans le graph
-graphE.namespace_manager.bind('mv', mv, override=False)
-graphE.namespace_manager.bind('geo', geo, override=False)
-graphE.namespace_manager.bind('ex', ex, override=False)
-graphP.namespace_manager.bind('mv', mv, override=False)
-graphP.namespace_manager.bind('geo', geo, override=False)
-graphP.namespace_manager.bind('ex', ex, override=False)
+    # Création des graphes
+    graphE = Graph(identifier="ElectricChargers")
+    graphP = Graph(identifier="Parkings")
 
-# RDF Chargeurs
-for row in df_bornes:
-    row = row['fields']
-    station_id = Literal(row['plugs_chargepointid'].replace('*', ''))
-    name_station = Literal(row['static_name'], datatype=XSD.string)
-    latitude = Literal(
-        row['geolocation_coordinates'][0],  datatype=XSD.decimal)
-    longitude = Literal(
-        row['geolocation_coordinates'][1],  datatype=XSD.decimal)
-    
-    status = Literal(row['plugs_status'], datatype=XSD.string)
-    plug_model = Literal(
-        row['plugs_outletmodel'], datatype=XSD.string)
-    blank_node = BNode()
+    # Création des namespaces dans le graph
+    graphE.namespace_manager.bind('mv', mv, override=False)
+    graphE.namespace_manager.bind('geo', geo, override=False)
+    graphE.namespace_manager.bind('ex', ex, override=False)
+    graphP.namespace_manager.bind('mv', mv, override=False)
+    graphP.namespace_manager.bind('geo', geo, override=False)
+    graphP.namespace_manager.bind('ex', ex, override=False)
 
-    item = URIRef(
-        'http://www.semweb.com/project/electric_charger#' + station_id)
+    # RDF Chargeurs
+    for row in df_bornes:
+        row = row['fields']
+        station_id = Literal(row['plugs_chargepointid'].replace('*', ''))
+        name_station = Literal(row['static_name'], datatype=XSD.string)
+        latitude = Literal(
+            row['geolocation_coordinates'][0],  datatype=XSD.decimal)
+        longitude = Literal(
+            row['geolocation_coordinates'][1],  datatype=XSD.decimal)
+        
+        status = Literal(row['plugs_status'], datatype=XSD.string)
+        plug_model = Literal(
+            row['plugs_outletmodel'], datatype=XSD.string)
+        blank_node = BNode()
 
-    # Création des triplets (statics)
-    graphE.add((item, mv.id, station_id))
-    graphE.add((item, mv.name, name_station))
-    graphE.add((item, geo.latitude, latitude))
-    graphE.add((item, geo.longitude, longitude))
+        item = URIRef(
+            'http://www.semweb.com/project/electric_charger#' + station_id)
 
-    # Création des triplets (dynamiques)
-    graphE.add((item, ex.hasPlug, blank_node))
-    graphE.add((blank_node, mv.PlugType, plug_model))
-    graphE.add((blank_node, ex.status, status))
-    if row['plugs_status'] == 'Disponible':
-        graphE.add((blank_node, mv.cableAvailable, Literal(True, datatype=XSD.boolean))) # DOIT ÊTRE BOOLEAN
-    else:
-        graphE.add((blank_node, mv.cableAvailable, Literal(False, datatype=XSD.boolean))) # DOIT ÊTRE BOOLEAN
+        # Création des triplets (statics)
+        graphE.add((item, mv.id, station_id))
+        graphE.add((item, mv.name, name_station))
+        graphE.add((item, geo.latitude, latitude))
+        graphE.add((item, geo.longitude, longitude))
 
-graphE.serialize(destination="df_bornes.ttl", format="turtle")
+        # Création des triplets (dynamiques)
+        graphE.add((item, ex.hasPlug, blank_node))
+        graphE.add((blank_node, mv.PlugType, plug_model))
+        graphE.add((blank_node, ex.status, status))
+        if row['plugs_status'] == 'Disponible':
+            graphE.add((blank_node, mv.cableAvailable, Literal(True, datatype=XSD.boolean))) # DOIT ÊTRE BOOLEAN
+        else:
+            graphE.add((blank_node, mv.cableAvailable, Literal(False, datatype=XSD.boolean))) # DOIT ÊTRE BOOLEAN
 
-# RDF stationnements
-for row in df_parking:
-    row = row['fields']
-    station_id = Literal(row['facilityid'])
-    name_station = Literal(row['nom_parking'], datatype=XSD.string)
-    latitude = Literal(row['geo'][0],  datatype=XSD.decimal)
-    longitude = Literal(row['geo'][1],  datatype=XSD.decimal)
+    graphE.serialize(format="json-ld", indent=4, destination="df_bornes.json")
+    graphE.serialize(destination="df_bornes.ttl", format="turtle")
 
-    freePlaces = Literal(row['counterfreeplaces'], datatype=XSD.integer)
-    typePlaces = Literal(row['countertype'], datatype=XSD.string)
-    blank_node = BNode()
+    # RDF stationnements
+    for row in df_parking:
+        row = row['fields']
+        station_id = Literal(row['facilityid'])
+        name_station = Literal(row['nom_parking'], datatype=XSD.string)
+        latitude = Literal(row['geo'][0],  datatype=XSD.decimal)
+        longitude = Literal(row['geo'][1],  datatype=XSD.decimal)
 
-    item = URIRef('http://www.semweb.com/project/parking_station#' + station_id)
+        freePlaces = Literal(row['counterfreeplaces'], datatype=XSD.integer)
+        typePlaces = Literal(row['countertype'], datatype=XSD.string)
+        blank_node = BNode()
 
-    # Création des triplets (statics)
-    graphP.add((item, mv.id, station_id))
-    graphP.add((item, mv.name, name_station))
-    graphP.add((item, geo.latitude, latitude))
-    graphP.add((item, geo.longitude, longitude))
-    
-    # Création des triplets (dynamique)
-    graphP.add((item, ex.hasParking, blank_node))
-    graphP.add((blank_node, mv.RealTimeCapacity, freePlaces))
-    graphP.add((blank_node, mv.ParkingFacility, typePlaces))
-    # TODO: associer real time capacity with its corresponding time
-    
-graphP.serialize(destination="df_parking.ttl", format="turtle")
+        item = URIRef('http://www.semweb.com/project/parking_station#' + station_id)
 
-# Automatisation de l'upload sur Fuseki 
-chargeurs_data = MultipartEncoder(fields={'file': ('df_bornes.ttl', open('df_bornes.ttl', 'rb'), 'text/turtle')}) 
-parking_data = MultipartEncoder(fields={'file': ('df_parking.ttl', open('df_parking.ttl', 'rb'), 'text/turtle')}) 
-put_bornes = requests.put('http://localhost:3030/ds/data?graph=http://www.semweb.com/project/electric_charger#', data=chargeurs_data, auth=('admin','mypassword'),headers={'Content-Type': chargeurs_data.content_type})
-put_parking = requests.put('http://localhost:3030/ds/data?graph=http://www.semweb.com/project/parking_station#', data=parking_data, auth=('admin','mypassword'),headers={'Content-Type': parking_data.content_type})
+        # Création des triplets (statics)
+        graphP.add((item, mv.id, station_id))
+        graphP.add((item, mv.name, name_station))
+        graphP.add((item, geo.latitude, latitude))
+        graphP.add((item, geo.longitude, longitude))
+        
+        # Création des triplets (dynamique)
+        graphP.add((item, ex.hasParking, blank_node))
+        graphP.add((blank_node, mv.RealTimeCapacity, freePlaces))
+        graphP.add((blank_node, mv.ParkingFacility, typePlaces))
+        # TODO: associer real time capacity with its corresponding time
+        
+    graphE.serialize(format="json-ld", indent=4, destination="df_parking.json")
+    graphP.serialize(destination="df_parking.ttl", format="turtle")
 
+    # Automatisation de l'upload sur Fuseki 
+    chargeurs_data = MultipartEncoder(fields={'file': ('df_bornes.ttl', open('df_bornes.ttl', 'rb'), 'text/turtle')}) 
+    parking_data = MultipartEncoder(fields={'file': ('df_parking.ttl', open('df_parking.ttl', 'rb'), 'text/turtle')}) 
+    put_bornes = requests.put('http://localhost:3030/ds/data?graph=http://www.semweb.com/project/electric_charger#', data=chargeurs_data, auth=('admin','mypassword'),headers={'Content-Type': chargeurs_data.content_type})
+    put_parking = requests.put('http://localhost:3030/ds/data?graph=http://www.semweb.com/project/parking_station#', data=parking_data, auth=('admin','mypassword'),headers={'Content-Type': parking_data.content_type})
+
+loadDatatoServer()
 # Preliminar SPARQL
 # prefix xsd: <http://www.w3.org/2001/XMLSchema#>
 
